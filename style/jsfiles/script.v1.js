43b0e5b7cbff351b8da0153f81a47024c0cc69da
Quick Coding Guides <38427168+Quick-Coding-Guides@users.noreply.github.com>
2019-01-10 02:02:49 +0100

    Temp Converter Upload

---
+++ style/jsfiles/script.v1.js
@@ -0,0 +1,28 @@
+// Calculator
+
+// Temp Calculator
+function TemperatureConverter(source,valNum) {
+    valNum = parseFloat(valNum);
+    var inputFahrenheit = document.getElementById("inputFahrenheit");
+    var inputCelsius = document.getElementById("inputCelsius");
+    var inputKelvin = document.getElementById("inputKelvin");
+    if (source=="inputFahrenheit") {
+        inputCelsius.value=((valNum-32)/1.8).toFixed(2);
+        inputKelvin.value=(((valNum-32)/1.8)+273.15).toFixed(2);
+    }
+    if (source=="inputCelsius") {
+        inputFahrenheit.value=((valNum*1.8)+32).toFixed(2);
+        inputKelvin.value=((valNum)+273.15).toFixed(2);
+    }
+    if (source=="inputKelvin") {
+        inputFahrenheit.value=(((valNum-273.15)*1.8)+32).toFixed(2);
+        inputCelsius.value=((valNum)-273.15).toFixed(2);
+    }
+}
+
+// Clear Value
+function ClearValue() {
+    document.getElementById('inputKelvin').value = '';
+    document.getElementById('inputFahrenheit').value = '';
+    document.getElementById('inputCelsius').value = '';
+}
